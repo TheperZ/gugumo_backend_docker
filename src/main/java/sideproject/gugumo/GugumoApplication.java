b6069b397ff694f0581f0960c717e897ceec04e4
HeeSeong Park <donald010@naver.com>
2024-04-14 21:07:02 +0900

    init project

---
+++ src/main/java/sideproject/gugumo/GugumoApplication.java
@@ -0,0 +1,13 @@
+package sideproject.gugumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GugumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GugumoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sideproject/gugumo/GugumoApplicationTests.java
@@ -0,0 +1,13 @@
+package sideproject.gugumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GugumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
