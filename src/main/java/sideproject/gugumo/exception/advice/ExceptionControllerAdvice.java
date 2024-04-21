21b92219b77c17dbb3c846f2e3728ec62d2ebbb2
HeeSeong Park <donald010@naver.com>
2024-04-21 23:23:59 +0900

    ExceptionControllerAdvice 추가
    Exception 관리 클래스 추가

---
+++ src/main/java/sideproject/gugumo/exception/advice/ExceptionControllerAdvice.java
@@ -0,0 +1,27 @@
+package sideproject.gugumo.exception.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import sideproject.gugumo.exception.DuplicateEmailException;
+import sideproject.gugumo.response.ApiResponse;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionControllerAdvice {
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler
+    public ApiResponse<String> duplicateEmailExceptionHandler(DuplicateEmailException e) {
+        log.error("[exceptionHandler] ex : " + e.getMessage());
+        return ApiResponse.createFail(e.getMessage());
+    }
+
+    @ExceptionHandler
+    public ApiResponse<String> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
+        log.error("[exceptionHandler] ex : " + e.getMessage());
+        return ApiResponse.createFail("입력이 올바르지 못합니다.");
+    }
+}
