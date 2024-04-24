d995b57683513e8d4478998d8bd373fac6c4236e
HeeSeong Park <donald010@naver.com>
2024-04-25 00:10:13 +0900

    MpaStruct 라이브러리 일시 사용 중지
    MapStruct는  default 값을 지정하는 방법을 찾지 못해 잠시 사용 보류

--- src/test/java/sideproject/gugumo/domain/dto/MemberMapperTest.java
+++ src/test/java/sideproject/gugumo/domain/dto/MemberMapperTest.java
@@ -13,14 +13,14 @@ class MemberMapperTest {
     @DisplayName("SignupMemberDto에서 Member로 변환이 가능해야 한다.")
     public void SignupToMemberTest() {
         //given
-        SignUpMemberDto signUpMemberDto = new SignUpMemberDto();
-        signUpMemberDto.setEmail("email");
-        signUpMemberDto.setPassword("password");
-        signUpMemberDto.setNickname("nickname");
+//        SignUpMemberDto signUpMemberDto = new SignUpMemberDto();
+//        signUpMemberDto.setEmail("email");
+//        signUpMemberDto.setPassword("password");
+//        signUpMemberDto.setNickname("nickname");
 
         //when
-        Member member = MemberMapper.INSTANCE.toEntity(signUpMemberDto, MemberStatus.active);
-        System.out.println("member = " + member);
+//        Member member = MemberMapper.INSTANCE.toEntity(signUpMemberDto, MemberStatus.active);
+//        System.out.println("member = " + member);
 
         //than
 
