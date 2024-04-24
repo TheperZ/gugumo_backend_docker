30787dfa5c9824552521dccbb1ca2af322f556ee
HeeSeong Park <donald010@naver.com>
2024-04-25 00:10:40 +0900

    어노테이션 및 import문 일부 수정

--- src/main/java/sideproject/gugumo/domain/entity/MemberStatus.java
+++ src/main/java/sideproject/gugumo/domain/entity/MemberStatus.java
@@ -1,8 +1,5 @@
 package sideproject.gugumo.domain.entity;
 
-import jakarta.persistence.Embeddable;
-import jakarta.persistence.Enumerated;
-
 public enum MemberStatus {
     active, inactive, delete
 }
