5e51d3bc7b4cc295ce1e6a332fe0ef98f89e7cc1
HeeSeong Park <donald010@naver.com>
2024-04-29 23:20:49 +0900

    권한 예외 추가 및 일부 로그 수정

--- src/main/java/sideproject/gugumo/exception/advice/ExceptionControllerAdvice.java
+++ src/main/java/sideproject/gugumo/exception/advice/ExceptionControllerAdvice.java
@@ -3,26 +3,42 @@ package sideproject.gugumo.exception.advice;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import sideproject.gugumo.exception.DuplicateEmailException;
+import sideproject.gugumo.exception.NoAuthorizationException;
 import sideproject.gugumo.response.ApiResponse;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
     @ResponseStatus(HttpStatus.CONFLICT)
-    @ExceptionHandler
+    @ExceptionHandler()
     public ApiResponse<String> duplicateEmailExceptionHandler(DuplicateEmailException e) {
-        log.error("[exceptionHandler] ex : " + e.getMessage());
+        log.error("[duplicateEmailExceptionHandler] ex : " + e.getMessage());
         return ApiResponse.createFail(e.getMessage());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler
     public ApiResponse<String> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
-        log.error("[exceptionHandler] ex : " + e.getMessage());
+        log.error("[MethodArgumentNotValidExceptionHandler] ex : " + e.getMessage());
         return ApiResponse.createFail("입력이 올바르지 못합니다.");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ApiResponse<String> MissingRequestHeaderExceptionHandler(MissingRequestHeaderException e) {
+        log.error("[MissingRequestHeaderExceptionHandler] ex : " + e.getMessage());
+        return ApiResponse.createFail("권한이 없습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler
+    public ApiResponse<String> NoAuthorizationExceptionHandler(NoAuthorizationException e) {
+        log.error("[NoAuthorizationExceptionHandler] ex : " + e.getMessage());
+        return ApiResponse.createFail(e.getMessage());
     }
 }
