9cf2df95c80a2e9bfca1b88f565c656bc933d53e
HeeSeong Park <donald010@naver.com>
2024-04-29 23:14:30 +0900

    로그인 기능 추가

---
+++ src/main/java/sideproject/gugumo/jwt/JwtFilter.java
@@ -0,0 +1,63 @@
+package sideproject.gugumo.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import sideproject.gugumo.domain.dto.CustomUserDetails;
+import sideproject.gugumo.domain.entity.Member;
+import sideproject.gugumo.domain.entity.MemberRole;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization == null || !authorization.startsWith("Bearer ")) {
+            System.out.println("token null");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+//        System.out.println("authorization now");
+        String token = authorization.split(" ")[1];
+
+        if (jwtUtil.isExpired(token)) {
+            System.out.println("token expired");
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+        String username = jwtUtil.getUsername(token);
+        String role = jwtUtil.getRole(token);
+
+        Member member = Member.createMemberBuilder()
+                .role(role)
+                .nickname("nickname")
+                .username(username)
+                .password("pass")
+                .build();
+
+        CustomUserDetails customUserDetails = new CustomUserDetails(member);
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
