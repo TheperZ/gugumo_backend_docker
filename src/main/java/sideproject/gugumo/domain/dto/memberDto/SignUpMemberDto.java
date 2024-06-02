a88389214ecbec9ad699f50cf490214ec5b449ea
HeeSeong Park <donald010@naver.com>
2024-06-02 12:23:23 +0900

    회원가입 DTO 통합
    SignUpMemberWithEmailDto 삭제

--- src/main/java/sideproject/gugumo/domain/dto/memberDto/SignUpMemberDto.java
+++ src/main/java/sideproject/gugumo/domain/dto/memberDto/SignUpMemberDto.java
@@ -10,11 +10,6 @@ public class SignUpMemberDto {
     private String nickname;
     private String username;
     private String password;
-
-    @Builder
-    public SignUpMemberDto(String nickname, String username, String password) {
-        this.nickname = nickname;
-        this.username = username;
-        this.password = password;
-    }
+    private String favoriteSports;
+    private String emailAuth;
 }
