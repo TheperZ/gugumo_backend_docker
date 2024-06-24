afbd5e07ee09448b16bbd86d04439d6d9b0e1faf
heeseong <donald010@naver.com>
2024-06-25 00:53:24 +0900

    Update stop.sh
    "로 인한 에러 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -6,9 +6,9 @@ STOP_LOG="$ROOT_PATH/stop.log"
 SERVICE_PID=$(pgrep -f $JAR) # 실행중인 Spring 서버의 PID
 
 if [ -z "$SERVICE_PID" ]; then
-  echo "[$NOW] > "서비스 NouFound" >> $STOP_LOG
+  echo "[$NOW] > 서비스 NouFound" >> $STOP_LOG
 else
-  echo "[$NOW] > "서비스 종료 " >> $STOP_LOG
+  echo "[$NOW] > 서비스 종료 " >> $STOP_LOG
   kill "$SERVICE_PID"
   # kill -9 $SERVICE_PID # 강제 종료를 하고 싶다면 이 명령어 사용
 fi
