7c82ca04207c588de3b4f8f9291f240f67b90563
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-07-08 02:17:33 +0900

    빈 fcm 토큰을 허용하지 않도록 수정

--- src/main/java/sideproject/gugumo/request/FcmTokenDto.java
+++ src/main/java/sideproject/gugumo/request/FcmTokenDto.java
@@ -1,11 +1,12 @@
 package sideproject.gugumo.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class FcmTokenDto {
 
-
+    @NotEmpty
     private String fcmToken;
 }
