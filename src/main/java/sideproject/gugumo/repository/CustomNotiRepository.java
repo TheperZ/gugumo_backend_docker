ca0c0add1ad817b16ddab5263e3f9b38030f2721
TheperZ <94631956+TheperZ@users.noreply.github.com>
2024-07-16 02:17:15 +0900

    벌크 삭제 함수 수정

--- src/main/java/sideproject/gugumo/repository/CustomNotiRepository.java
+++ src/main/java/sideproject/gugumo/repository/CustomNotiRepository.java
@@ -1,7 +1,11 @@
 package sideproject.gugumo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import sideproject.gugumo.domain.entity.notification.CustomNoti;
 import sideproject.gugumo.domain.entity.member.Member;
 
@@ -11,5 +15,9 @@ import java.util.List;
 public interface CustomNotiRepository extends JpaRepository<CustomNoti, Long> {
     public List<CustomNoti> findByMemberOrderByCreateDateDesc(Member member);
 
-    public void deleteAllInBatchByMemberAndIsReadTrue(Member member);
+    @Modifying(clearAutomatically = true)
+    @Query("delete from CustomNoti c where c.member=:member and c.isRead=true")
+    @Transactional
+    public void deleteAllByMemberAndIsReadTrue(@Param("member") Member member);
+
 }
--- src/main/java/sideproject/gugumo/repository/FcmNotificationTokenRepository.java
+++ src/main/java/sideproject/gugumo/repository/FcmNotificationTokenRepository.java
@@ -1,7 +1,11 @@
 package sideproject.gugumo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import sideproject.gugumo.domain.entity.notification.FcmNotificationToken;
 import sideproject.gugumo.domain.entity.member.Member;
 
@@ -17,7 +21,10 @@ public interface FcmNotificationTokenRepository extends JpaRepository<FcmNotific
 
     public Optional<FcmNotificationToken> findByMemberAndToken(Member member, String token);
 
-    public void deleteAllByToken(String token);
+    @Modifying(clearAutomatically = true)
+    @Query("delete from FcmNotificationToken t where t.token=:token")
+    @Transactional
+    public void deleteAllByToken(@Param("token") String token);
 
     public List<FcmNotificationToken> findByMember(Member member);
 
